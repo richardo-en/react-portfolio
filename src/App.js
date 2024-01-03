ca45a0bdbe6958c6d838158f7ae705b5fd606d9a
richardo-en <richard3.nemeth@gmail.com>
2024-01-03 10:08:06 +0100

    links fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useReducer } from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import LanguageContext from './additional_components/language_context.js';
 import languageReducer from './additional_components/Language_reducer.js';
 
-import Freelancing from './sites/freelancing_page.js';
+// import Freelancing from './sites/freelancing_page.js';
 import LandingPage from './sites/landing_page.js';
 import Certificates from './sites/certificates_page.js';
 import ProjectsPage from './sites/projects_page.js';
@@ -24,7 +24,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<LandingPage />} />
-          <Route path="/freelance" element={<Freelancing />} />
+          {/* <Route path="/freelance" element={<Freelancing />} /> */}
           <Route path="/certificate" element={<Certificates />} />
           <Route path="/projects" element={<ProjectsPage/>} />
         </Routes>
